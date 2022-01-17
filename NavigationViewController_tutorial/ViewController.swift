9650c4d9da8b1514a8f905efe2d969f04ffca6da
toto <toto5032@gmail.com>
2022-01-17 15:11:39 +0900

    Initial Commit

---
+++ NavigationViewController_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationViewController_tutorial
+//
+//  Created by admin on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
